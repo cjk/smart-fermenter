349b52c076c9bf733f897596d28f25f44afbeda7
Claus Klingberg <cjk@pobox.com>
2016-03-03 14:01:01 +0100

    Allow devices to run max. 15 min before shutting down.

--- app/watchdogs/deviceRunningTooLong.js
+++ app/watchdogs/deviceRunningTooLong.js
@@ -2,7 +2,7 @@
    in a bad state. */
 import notify from '../notifications';
 
-const durationLimit = 10000 * 60; /* Ten minutes */
+const durationLimit = 15000 * 60; /* allow devices to run at most fifteen minutes */
 
 /* To send notifications */
 const messenger = notify();
