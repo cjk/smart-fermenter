49ab37519dc9760084cef69363ae40f918767ebf
Claus Klingberg <cjk@pobox.com>
2016-04-24 11:45:30 +0200

    Change Websocket-channel names to new schema start listening to incoming fermenter-commands (WIP).

--- app/server/index.js
+++ app/server/index.js
@@ -3,13 +3,21 @@ import IO from 'socket.io';
 const io = IO(8080);
 
 function emitFermenterState(s) {
-  io.emit('tempHumidity', s);
+  io.emit('fermenterState', s);
 }
 
 function init(stream) {
   io.on('connection', (socket) => {
-    socket.on('tempHumidity', (request) => {
-      console.log(`~~ I received a tempHumidity-request from ${request.from}:`);
+    console.log('~~ Client connected via SocketIO.');
+
+    socket.on('fermenterCommand', (cmd) => {
+      console.log(`~~ Received a remote-command: <${cmd}>`);
+      //stream.offValue(emitFermenterState);
+    });
+
+    /* Subscribe to state-requests from the backend */
+    socket.on('fermenterState', (request) => {
+      console.log(`~~ I received a fermenterState-request from ${request.from}:`);
       stream.onValue(emitFermenterState);
     });
 
