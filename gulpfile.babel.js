928cc4f293b900dbad8f0823a1b94d2497825c59
Claus Klingberg <cjk@pobox.com>
2016-08-03 12:28:14 +0200

    Add clean-task, source-maps and rebuild only changed files.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,22 +1,37 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
-import watch from 'gulp-watch';
+import changed from 'gulp-changed';
 import preprocess from 'gulp-preprocess';
+import sourcemaps from 'gulp-sourcemaps';
+import watch from 'gulp-watch';
+import del from 'del';
+
+const SRC = './src/**/*.js';
+const DEST = 'app';
 
 function build() {
-  gulp.src(['./src/**/*.js'])
+  gulp.src([SRC])
+      .pipe(changed(DEST))
+      .pipe(sourcemaps.init())
       .pipe(preprocess())
       .pipe(babel({
         presets: ['es2015-node5', 'stage-1'],
         plugins: ['transform-runtime', 'add-module-exports']
       }))
-      .pipe(gulp.dest('app'));
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(DEST));
 }
+
+gulp.task('clean', () =>
+  del([
+    DEST,
+  ])
+);
 
 gulp.task('build', build);
 
 gulp.task('hotupdate', () =>
-  watch('src/**/*.js', build, {ignoreInitial: false})
+  watch(SRC, build, {ignoreInitial: false})
 );
 
 gulp.task('default', ['build', 'hotupdate']);
