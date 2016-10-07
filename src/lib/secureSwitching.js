f234a16ad6db86af359a7600b80e12532758893c
Claus Klingberg <cjk@pobox.com>
2016-10-07 20:25:15 +0200

    Limit switches to max 3 but make timing more random to avoid intermittent / lost switches.

--- src/lib/secureSwitching.js
+++ src/lib/secureSwitching.js
@@ -2,16 +2,20 @@ import K from 'kefir';
 import {getRandomInt} from './random';
 
 const intervalSwitch = () => {
+  let switchCount = 0;
   const start = new Date();
-  const randomDelay = getRandomInt(10, 50);
+  const randomDelay = getRandomInt(1, 1000);
 
-  return K.withInterval(750 + randomDelay, (emitter) => {
+  return K.withInterval(1000 + randomDelay, (emitter) => {
     const time = new Date() - start;
 
-    /* Allow a max. time of 4 seconds for switching-actions to take place. Make sure this fits nicely into how often
+    /* Allow a max. time of 4 seconds for switching-actions to take place.
+     * Also don't switch more than 3 times.
+     * NOTE: Make sure this fits nicely into how often
        sensor-events are emitted! */
-    if (time < 4000) {
+    if (time < 6000 && switchCount < 3) {
       emitter.emit(time);
+      switchCount += 1;
     } else {
       emitter.end();
     }
