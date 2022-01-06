d82721ca66a4b039ae9473d2e0c52cd8183bdd27
Claus Klingberg <cjk@pobox.com>
2022-01-06 18:12:08 +0100

    Provide ability to switch fermenter on/off using system-signaling

--- src/actors/bootstrapRuntimeState.js
+++ src/actors/bootstrapRuntimeState.js
@@ -4,13 +4,36 @@ import type { FermenterState } from '../types.js'
 import * as R from 'ramda'
 import signale from 'signale'
 
+// This block allows us the toggle switch the fermenter on/off from a local console
+var toggleOnOff = false
+// Listen to local system-signal and activate toggleOnOff-variable if received
+process.on('SIGUSR2', () => {
+  signale.info('Received SIGUSR2 from system...')
+  toggleOnOff = true
+})
+// Reacts to process-signaling by either setting currentCmd to 'start' or 'stop' here depending on if fermenter was on
+// or off before
+const maybeToggleFermenterFromSignal = (isActive, currentCmd) => {
+  // NOTE we only interfere if there is no competing command already being sent;
+  // Also make sure we always return a non-undefined state, even if we received an empty one
+  if (!toggleOnOff || currentCmd != 'none') return currentCmd
+  toggleOnOff = false
+  return isActive ? 'stop' : 'start'
+}
+
 function bootstrapRuntimeState(prev: FermenterState, curr: FermenterState) {
   const prevRts = prev.rts
+
+  // Set currentCmd. NOTE it may have been set remotely already (using peer / synced state) so we try to preserve this
+  // value. However, if the current command is empty / 'none', we allow it to be set from the console by sending SIGUSR2
+  // (see above)
+  const newCmd = maybeToggleFermenterFromSignal(prevRts.active, curr.rts.currentCmd ?? 'none')
 
   /* Carry over current fermenter-is-active state from previous state */
   const rts = R.merge(curr.rts, {
     active: prevRts.active,
     status: prevRts.status === 'initializing' ? (prevRts.active ? 'running' : 'off') : prevRts.status,
+    currentCmd: newCmd,
   })
 
   const updateStateWith = (rts, devices = curr.devices) => R.merge(curr, { rts, devices })
@@ -25,7 +48,7 @@ function bootstrapRuntimeState(prev: FermenterState, curr: FermenterState) {
         const newRts = R.merge(rts, {
           active: true,
           status: 'running',
-          currentCmd: null,
+          currentCmd: 'none',
           notifications: { [Date.now()]: { level: 'info', msg: 'Fermenter was started' } },
         })
         return updateStateWith(newRts)
@@ -36,7 +59,7 @@ function bootstrapRuntimeState(prev: FermenterState, curr: FermenterState) {
       if (rts.active) {
         /* Make sure we switch all devices off as well as the fermenter-closet! */
         const devices = R.map(
-          dev =>
+          (dev) =>
             R.merge(dev, {
               shouldSwitchTo: 'off',
               willSwitch: true,
@@ -48,7 +71,7 @@ function bootstrapRuntimeState(prev: FermenterState, curr: FermenterState) {
         const newRts = R.merge(rts, {
           active: false,
           status: 'off',
-          currentCmd: null,
+          currentCmd: 'none',
           notifications: { [Date.now()]: { level: 'info', msg: 'Fermenter was stopped' } },
         })
         return updateStateWith(newRts, devices)
--- src/controller/index.js
+++ src/controller/index.js
@@ -5,12 +5,7 @@ import temperatureController from './temperatureController.js'
 import humidityController from './humidityController.js'
 
 const iterationsLens = R.lensPath(['env', 'iterations'])
-
-const incIteration = state$ =>
+const incIteration = (state$) =>
   state$.scan((prev, next) => R.set(iterationsLens, R.view(iterationsLens, prev) + 1, next))
 
-export default R.pipe(
-  incIteration,
-  temperatureController,
-  humidityController
-)
+export default R.pipe(incIteration, temperatureController, humidityController)
